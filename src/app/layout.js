28252431aa8a01315efe884fa169bb4bf2f45d47
abdulmalek.swe.585@gmail.com <abdulmale.swe.585@gmail.com>
2024-07-10 22:24:59 +0600

    fix:some comment in navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
-import NextProgress from "next-progress";
-import PogressBar from "@/lib/PogressBar";
 import Footer from "@/components/footer/Footer";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,25 +15,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-     {/* <PogressBar/> */}
-    
-     <Navbar/>
-     <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
- 
-/>
+        {/* <PogressBar/> */}
+        <Navbar />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
   );
 }
